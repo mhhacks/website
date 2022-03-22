6077a835d6cab9a342ed5b17d4d44d84b4dabc50
Benjamin Ashbaugh <hi@benjaminashbaugh.me>
2022-03-22 15:01:24 -0600

    prospectuslol

--- next.config.js
+++ next.config.js
@@ -15,6 +15,11 @@ module.exports = withMDX({
         source: '/register',
         destination: '/preregister',
         permanent: false,
+      },
+      {
+        source: '/prospectus',
+        destination: '/MHHacks_v0_Sponsorship_Prospectus_20220322.pdf',
+        permanent: false
       }
     ]
   },
